fc02774eec2f3c61cdaeede7e702d50de7d050d6
Andrew Krygoski <andrew.krygoski@gmail.com>
2018-01-14 15:01:53 -0500

    second commit, added translation

--- seePlusPlus/main.cpp
+++ seePlusPlus/main.cpp
@@ -1,11 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <Windows.h>
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1200, 300), "Yo check this garbo!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
-	
+	bool first = true;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -15,9 +20,24 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		struct timespec req = { 0 };
+		req.tv_nsec = 0;
+		req.tv_sec = 2;
+
+		if (first) 
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				window.clear();
+				cout << shape.getPosition().x << "," << shape.getPosition().y << endl;
+				shape.setPosition(shape.getPosition().x + 1, 0);
+				window.draw(shape);
+				window.display();
+				//Sleep(1000);
+			}
+			first = false;
+		}
+		
 	}
 
 	return 0;
